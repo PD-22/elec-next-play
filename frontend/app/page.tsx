e1489b37d4b0ccfabc30cb1d54eb8b73e02a4c46
Pavle <p.devidze@gmail.com>
2024-11-06 17:01:32 +0400

    get subtitles

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,19 +1,41 @@
 'use client'
 
-import styles from './page.module.css'
+import { useState } from "react";
+
+const initUrl = 'GpI68hQ3acM';
 
 export default function Home() {
-	const handleFetch = async () => {
-		const date = new Date().toISOString()
-		const output = await window.ipc.invoke('write-date', date)
-		console.log(`write "${date}" to "${output}"`)
-	}
+	const [url, setUrl] = useState(initUrl);
+	const [subtitles, setSubtitles] = useState('');
+
+	const clickHandler = async () => {
+		try {
+			setSubtitles('Loading...');
+			const data = await window.ipc.invoke('get-subtitles', url);
+			console.log(data);
+			const newSubtitles = data
+				.filter((x: any) => x.line)
+				.map((x: any) => `[${x.startTime}] ${x.line}`)
+				.join('\n');
+			if (!newSubtitles)
+				throw new Error(`Invalid subtitles "${newSubtitles}"`);
+			setSubtitles(newSubtitles);
+		} catch (error) {
+			console.error(error);
+			setSubtitles('Error');
+		}
+	};
 
 	return (
-		<div className={styles.wrapper}>
-			<button className="border p-2" onClick={handleFetch} autoFocus>
-				Write Date to Desktop
-			</button>
+		<div>
+			<input
+				value={url}
+				onChange={e => setUrl(e.target.value)}
+				placeholder="https://www.youtube.com/watch?v="
+			/>
+			<button onClick={clickHandler}>Extract Subtitles</button>
+			<hr />
+			<pre>{subtitles}</pre>
 		</div>
 	)
 }
